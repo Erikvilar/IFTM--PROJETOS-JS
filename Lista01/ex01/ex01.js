493633733cbf56403541f6d72ea17f6ebfb6b460
Erikvilar <91624090+Erikvilar@users.noreply.github.com>
2023-08-14 17:11:17 -0300

    Add files via upload
    Lista01 - atividades

---
+++ Lista01/ex01/ex01.js
@@ -0,0 +1,6 @@
+nome = prompt("digite seu nome"); //le informaç.ões // o script e executado antes do Body em html
+                                  
+sobrenome = prompt("digite seu sobrenome");
+// alert("Seu nome e "+ nome+sobrenome);
+
+alert(`Nome completo = ${nome} ${sobrenome}`); //crase e sifrão{} concatena as variaveis
---
+++ Lista01/ex05/ex05.js
@@ -0,0 +1,6 @@
+nome = prompt("Qual seu nome?");
+var char = nome.length;
+
+
+document.write(`O seu nome e: ${nome} e possui ${char} caracteres`);
+
